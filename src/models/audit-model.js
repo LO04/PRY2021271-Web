de4f4469ff2b53f0a38b9e051baafd7325055a06
Sentra <osca-ru@hotmail.com>
2022-04-23 21:58:25 -0500

    fix deploy

--- src/models/audit-model.js
+++
@@ -1,8 +0,0 @@
-export default class AuditModel {
-    constructor(id, createdAt, updatedAt, isDeleted) {
-        this.id = id;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-        this.isDeleted = isDeleted;
-    }
-}
--- src/services/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export default axios.create({
-    baseURL: 'https://localhost:5001/api',
-    headers: {
-        'Content-type': 'application/json'
-    }
-});
